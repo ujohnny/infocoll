7b2432a929a12b55fedfe648204909e5365c3918
Eugeny Novozhilov <eugeny.novozhilov@gmail.com>
2012-07-11 14:23:34 +0400

    changed formatting for parsing data, swapped incorrect length and offset

--- utils/client/client.c
+++ utils/client/client.c
@@ -21,19 +21,18 @@ uint64_t extract_uint64(unsigned char *str)
 void process_data(struct nlmsghdr *nlh) {
 	unsigned char *payload = NLMSG_DATA(nlh);
 	unsigned char type = payload[0];
-	uint64_t length = extract_uint64(payload+1),
-		offset = extract_uint64(payload+9),
+	uint64_t offset = extract_uint64(payload+1),
+		length = extract_uint64(payload+9),
 		inode = extract_uint64(payload+17),
 		time_sec = extract_uint64(payload+25),
 		time_nsec = extract_uint64(payload+33);
-	printf("Rcvd msg: {type = %d, length = %llu, offset = %llu, inode = %llu, time = %llu.%llu}\n",
+	printf("%d\t%llu\t%llu\t%llu\t%llu.%llu\n",
 		   type, length, offset, inode, time_sec, time_nsec);
 	memset(payload, 0, MAX_PAYLOAD);
 }
 
 int main()
 {
-
 	int sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_INFOCOLL);
 	if(sock_fd < 0) {
 		printf("Device not mounted\n");
@@ -43,7 +42,7 @@ int main()
 	struct sockaddr_nl src_addr;
 	memset(&src_addr, 0, sizeof(src_addr));
 	src_addr.nl_family = AF_NETLINK;
-	src_addr.nl_pid = getpid(); /* self pid */
+	src_addr.nl_pid = getpid(); // self pid 
 
 	bind(sock_fd, (struct sockaddr*) &src_addr, sizeof(src_addr));
 
@@ -51,8 +50,8 @@ int main()
 	memset(&dst_addr, 0, sizeof(dst_addr));
 	memset(&dst_addr, 0, sizeof(dst_addr));
 	dst_addr.nl_family = AF_NETLINK;
-	dst_addr.nl_pid = 0; /* For Linux Kernel */
-	dst_addr.nl_groups = 0; /* unicast */
+	dst_addr.nl_pid = 0; // For Linux Kernel
+	dst_addr.nl_groups = 0; // unicast 
 	
 
 	struct nlmsghdr *nlh = (struct nlmsghdr *) malloc(NLMSG_SPACE(MAX_PAYLOAD));
@@ -60,8 +59,6 @@ int main()
 	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
 	nlh->nlmsg_pid = getpid();
 	nlh->nlmsg_flags = 0;
-	
-	strcpy(NLMSG_DATA(nlh), "llambda!");
 
 	struct iovec iov;
 	iov.iov_base = (void *) nlh;
@@ -74,11 +71,11 @@ int main()
 	msg.msg_iov = &iov;
 	msg.msg_iovlen = 1;
 
-	printf("Sending message to kernel\n");
-	sendmsg(sock_fd, &msg, 0);
-	printf("Waiting for message from kernel\n");
+	sendmsg(sock_fd, &msg, 0); // send msg to kernel with our pid 
 
-	/* Read message from kernel */
+	printf("type\tlength\toffset\tinode\ttime\n"); //header for output file
+
+	/* Reading messages from kernel */
 
 	do {
 		recvmsg(sock_fd, &msg, 0);
